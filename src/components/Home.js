704c60d2f5dfbdff855ef19277c45c0887aac6bf
rafrex <code@rafrex.com>
2017-05-02 18:17:21 -0700

    Implement react-interactive on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import Interactive from 'react-interactive';
 
 class Home extends React.Component {
   state = {
@@ -21,62 +22,77 @@ class Home extends React.Component {
   render() {
     return (
       <nav style={{ height: '100%', fontSize: '15vmin' }}>
-        <div style={{ height: '33%' }}><Link
-          to="/code" id="code"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-            backgroundColor: '#E0E0E0',
-          }}
-        >
-          Code
-        </Link></div>
-        <div style={{ height: '33%' }}><Link
-          to="/" id="photography" onClick={this.handlePhotoClick}
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-            backgroundColor: '#D0D0D0',
-          }}
-        >
-          <CSSTransitionGroup
-            transitionName="coming-soon"
-            transitionEnterTimeout={0}
-            transitionLeaveTimeout={0}
+        <div style={{ height: '33%' }}>
+          <Interactive
+            as={Link}
+            to="/code"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              backgroundColor: '#E0E0E0',
+            }}
+            hover={{ color: 'rgb(0, 168, 0)' }}
+            active="hover"
+            focusFromTab={{ backgroundColor: 'rgba(0, 168, 0, .2)' }}
+          >Code</Interactive>
+        </div>
+        <div style={{ height: '33%' }}>
+          <Interactive
+            as={Link}
+            to="/"
+            onClick={this.handlePhotoClick}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              backgroundColor: '#D0D0D0',
+            }}
+            hover={{ color: 'rgb(0, 168, 0)' }}
+            active="hover"
+            focusFromTab={{ backgroundColor: 'rgba(0, 168, 0, .3)' }}
           >
-            <span
-              key={this.state.photoText}
-              style={{
-                position: 'absolute',
-                width: '100%',
-                textAlign: 'center',
-                WebkitTransform: 'translate(-50%, -50%)',
-                MozTransfor: 'translate(-50%, -50%)',
-                OTransform: 'translate(-50%, -50%)',
-                msTransform: 'translate(-50%, -50%)',
-                transform: 'translate(-50%, -50%)',
-              }}
+            <CSSTransitionGroup
+              transitionName="coming-soon"
+              transitionEnterTimeout={0}
+              transitionLeaveTimeout={0}
             >
-              {this.state.photoText}
-            </span>
-          </CSSTransitionGroup>
-        </Link></div>
-        <div style={{ height: '34%' }}><Link
-          to="/contact" id="contact"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-            backgroundColor: '#C0C0C0',
-          }}
-        >
-          Contact
-        </Link></div>
+              <span
+                key={this.state.photoText}
+                style={{
+                  position: 'absolute',
+                  width: '100%',
+                  textAlign: 'center',
+                  WebkitTransform: 'translate(-50%, -50%)',
+                  MozTransfor: 'translate(-50%, -50%)',
+                  OTransform: 'translate(-50%, -50%)',
+                  msTransform: 'translate(-50%, -50%)',
+                  transform: 'translate(-50%, -50%)',
+                }}
+              >
+                {this.state.photoText}
+              </span>
+            </CSSTransitionGroup>
+          </Interactive>
+        </div>
+        <div style={{ height: '34%' }}>
+          <Interactive
+            as={Link}
+            to="/contact"
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+              backgroundColor: '#C0C0C0',
+            }}
+            hover={{ color: 'rgb(0, 168, 0)' }}
+            active="hover"
+            focusFromTab={{ backgroundColor: 'rgba(0, 168, 0, .4)' }}
+          >Contact</Interactive>
+        </div>
       </nav>
     );
   }
